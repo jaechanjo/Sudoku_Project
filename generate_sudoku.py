70e3adf8ba1e3c9dd405cb92b4604b2d5499df54
jaechanjo <jjc12223a@gmail.com>
2024-09-20 16:20:22 +0900

    Update sudoku game functions

---
+++ generate_sudoku.py
@@ -0,0 +1,20 @@
+import random
+
+def generate_sudoku():
+    
+    puzzle = []
+    # 0의 확률을 50%으로 고정
+    numbers = [0] * 9 + list(range(1, 10))
+    for _ in range(9):    
+        row = []
+        for _ in range(9):
+            #rand_int = random.randint(0, 9)
+            rand_int = random.choice(numbers)
+            row.append(rand_int)
+        puzzle.append(row)
+        
+    return puzzle
+
+if __name__ == __main__:
+    sudoku_puzzle = generate_sudoku()
+    print_board(sudoku_puzzle)
